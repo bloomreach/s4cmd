785e7e76142ea2b663c415e0a982874324bfc17f
Chou-han Yang <chyang@bloomreach.com>
2015-02-18 14:08:34 -0800

    Bump version for setup.py

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ from setuptools import setup, find_packages
 __author__ = "Chou-han Yang"
 __copyright__ = "Copyright 2014 BloomReach, Inc."
 __license__ = "http://www.apache.org/licenses/LICENSE-2.0"
-__version__ = "1.5.19"
+__version__ = "1.5.20"
 __maintainer__ = __author__
 __status__ = "Development"
 
