f65e92d0351a768cc5d1510e2e6359b5d960cc50
Navin Pai <lifeofnavin@gmail.com>
2018-05-24 04:52:32 +0530

    Updating copyrights

--- runtests.sh
+++ runtests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #
-# Copyright 2012 BloomReach, Inc.
+# Copyright 2012-2018 BloomReach, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
